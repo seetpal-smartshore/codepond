64800cf79418f14d0cbc3a8f4e838f157cace6f8
Mr. Pal <creators.guru@gmail.com>
2015-09-09 18:22:38 +0530

    moving app intializing from index.html to app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
- 
+var app = angular.module('dash', []);
+
+app.controller('mainCtrl', function(){
+	var	game	=	this;
+	game.testo	=	[];
+	
+	game.sbumit	=	function(game1){
+		console.log('saved');
+		//game.testo	=	angular.copy(game.cast);
+		game.testo.push(game.cast);
+		//game1.$setUntouched();
+		game1.$setPristine();
+	};
+	game.mint	=	[];
+});
